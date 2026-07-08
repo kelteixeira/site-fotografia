768a95f329daf1ceefc9e8347fe522c37a37fb97
kelteixeira <kelly.t@aluno.senai.br>
2026-07-07 21:52:11 -0300

    versao_final

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form");
+
+form.addEventListener(
+    "submit",
+    function (event) {
+        event.preventDefault();
+        let nome = document.getElementById("nome").value;
+        let tel = document.getElementById("tel").value;
+        let mensagem = document.getElementById("mensagem").value;
+        if (nome.trim() === " ") {
+            alert("Por favor, preencha seu nome!");
+        }
+        else if (tel.trim() === " ") {
+            alert("Por favor, preencha seu telefone!");
+        }
+        else if (mensagem.trim() === " ") {
+            alert("Por favor, preencha sua mensagem");
+        }
+        else {
+            const mensagemEnvio = "Olá, gostaria de mais informações!"
+                + "\n\n *Nome: *" + nome
+                + "\n\n *Telefone: *" + tel
+                + "\n\n *Qual sua dúvida? *" + mensagem
+
+            let linkWhats = `https://wa.me/5543991928501?text=${encodeURIComponent(mensagemEnvio)}`;
+
+                window.open(linkWhats, "_blank");
+        }
+    }
+)
